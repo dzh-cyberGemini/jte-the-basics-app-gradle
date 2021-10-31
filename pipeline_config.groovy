613372e8f17ce2c2e562e3ac320392b890a90320
Dzhaner Ahmed <77504842+dzh-cyberGemini@users.noreply.github.com>
2021-10-31 21:19:35 +0200

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,8 +2,7 @@ alow_scm_jenkinsfile = false
 
 libraries{
     gradle{
-        message = "This variable is from gradle-app!"
+        message = "This variable is from GRADLE-app repository!"
         number = 23
     }
-    sonarqube
 }
