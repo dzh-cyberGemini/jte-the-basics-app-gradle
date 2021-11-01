115234f7b11ef63b6488bbf199d677d2e26cf0b7
Dzhaner Ahmed <77504842+dzh-cyberGemini@users.noreply.github.com>
2021-11-01 13:12:56 +0200

    Test for message variable

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,7 +2,7 @@ alow_scm_jenkinsfile = false
 
 libraries{
     gradle{
-        String message = "HELLO from GRADLE-app repository! This is message variable."
+        message = "HELLO from GRADLE-app repository! This is message variable."
         int number = 20
     }
 }
