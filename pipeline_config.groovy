a20388afd3ac9f9791642faf92f19189585fcf2a
Dzhaner Ahmed <77504842+dzh-cyberGemini@users.noreply.github.com>
2021-11-01 14:13:16 +0200

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -3,6 +3,7 @@ alow_scm_jenkinsfile = false
 libraries{
     gradle{
         message = "HELLO from GRADLE-app repository! This is message variable."
+        int number;
         number = 5
     }
 }
